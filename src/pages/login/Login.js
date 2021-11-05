04c1877ece6f384e385323a11a2aaf44eb5e925e
nwaose victor <nwaosevictor1234@gmail.com>
2021-11-06 00:29:40 +0100

    added style to login.css

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,8 +3,25 @@ import "./Login.css";
 
 const Login = () => {
     return (
-        <div className="logins">
-            <h1 className="loginstext"> Login</h1>
+        <div className="log">
+            <div className="log-container">
+                <div className="log-detail">
+                    <h1> Login</h1>
+                    <div className="log-form">
+                        <input
+                            type="text"
+                            placeholder="enter your email"
+                            className="log-input"
+                        />
+                        <input
+                            type="password"
+                            placeholder="enter your password"
+                            className="log-input"
+                        />
+                        <button className="">Login</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
